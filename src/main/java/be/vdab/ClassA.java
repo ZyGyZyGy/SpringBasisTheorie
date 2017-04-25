80db9bd7e382bbd32737403d19cefd186f489c6d
Aleks <Aleks@LOK111-05>
2017-04-25 09:22:42 +0200

    4 CONFIGURATIEGEGEVENS LEZEN UIT XML - Setter Injection

--- src/main/java/be/vdab/ClassA.java
+++ src/main/java/be/vdab/ClassA.java
@@ -1,7 +1,57 @@
 package be.vdab;
 
+import java.io.File;
+import java.net.URL;
+import java.util.Locale;
+
 public class ClassA implements InterfaceA {
 
+    private URL koersenURL;
+    private Locale defaultTaalEnLand;
+    private File importData;
+    private BladRichtingInPrinter defaultBladRichting;
+    private String telefoonNrHelpDesk;
+
+    public URL getKoersenURL() {
+	return koersenURL;
+    }
+
+    public void setKoersenURL(URL koersenURL) {
+	this.koersenURL = koersenURL;
+    }
+
+    public Locale getDefaultTaalEnLand() {
+	return defaultTaalEnLand;
+    }
+
+    public void setDefaultTaalEnLand(Locale defaultTaalEnLand) {
+	this.defaultTaalEnLand = defaultTaalEnLand;
+    }
+
+    public File getImportData() {
+	return importData;
+    }
+
+    public void setImportData(File importData) {
+	this.importData = importData;
+    }
+
+    public BladRichtingInPrinter getDefaultBladRichting() {
+	return defaultBladRichting;
+    }
+
+    public void setDefaultBladRichting(BladRichtingInPrinter defaultBladRichting) {
+	this.defaultBladRichting = defaultBladRichting;
+    }
+
+    public String getTelefoonNrHelpDesk() {
+	return telefoonNrHelpDesk;
+    }
+
+    public void setTelefoonNrHelpDesk(String telefoonNrHelpDesk) {
+	this.telefoonNrHelpDesk = telefoonNrHelpDesk;
+    }
+    
     @Override
     public String getBoodschap() {
 	return "ClassA object";
--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -5,10 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 class Main {
 
     public static void main(String[] args) {
-	try (ClassPathXmlApplicationContext context =
+	try (ClassPathXmlApplicationContext context = 
 		new ClassPathXmlApplicationContext("container.xml")) {
-	    System.out.println(context.getBean(ClassA.class).getBoodschap());
-	    System.out.println(context.getBean(InterfaceB.class).getBoodschap());
+	    ClassA objectA = context.getBean(ClassA.class);
+	    System.out.println(objectA.getKoersenURL());
+	    System.out.println(objectA.getDefaultTaalEnLand().getDisplayName());
+	    System.out.println("Bestand bestaat: " + objectA.getImportData().exists());
+	    System.out.println(objectA.getDefaultBladRichting());
 	}
     }
 }
