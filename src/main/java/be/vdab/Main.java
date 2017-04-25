82066f7289e9fb8b1556321fa144b82485e731fa
Aleks <Aleks@LOK111-05>
2017-04-25 11:30:39 +0200

    5 SCOPE

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -7,25 +7,11 @@ class Main {
     public static void main(String[] args) {
 	try (ClassPathXmlApplicationContext context = 
 		new ClassPathXmlApplicationContext("container.xml")) {
-	    ClassA objectA = context.getBean(ClassA.class);
-	    System.out.println(objectA.getKoersenURL());
-	    System.out.println(objectA.getDefaultTaalEnLand().getDisplayName());
-	    System.out.println("Bestand bestaat: " + objectA.getImportData().exists());
-	    System.out.println(objectA.getDefaultBladRichting());
-	    System.out.println();
-	    
-	    ClassB objectB = context.getBean(ClassB.class); 
-	    System.out.println(objectB.getTelefoonNrHelpDesk()); 
-	    System.out.println(objectB.getAantalPogingenUpdateKlant()); 
-	    System.out.println();
-	    
-	    System.out.println(context.getBean(HelpdeskMedewerkers.class)); 
-	    System.out.println();
-	    
-	    System.out.println(context.getBean(ClassA.class).getWebMasterEmailAdres()); 
-	    System.out.println();
-	    
-	    System.out.println(context.getBean(ClassA.class).getWebsiteGestart()); 
+	    context.getBean("teller1", Teller.class).verhoog();
+	    context.getBean("teller1", Teller.class).verhoog();
+	    Teller teller2 = context.getBean("teller2", Teller.class);
+	    teller2.verhoog();
+	    context.getBean("teller2", Teller.class).verhoog();
 	}
     }
 }
