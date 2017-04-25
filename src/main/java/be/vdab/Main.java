5b1cafc64bc49fb6cf35e8d4bc6d0f23572bdc3b
Aleks <Aleks@LOK111-05>
2017-04-25 13:55:09 +0200

    8 BEANS DEFINIËREN MET ANNOTATIONS

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -2,18 +2,19 @@ package be.vdab;
 
 import java.math.BigDecimal;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import be.vdab.restclients.RestClientsConfig;
 import be.vdab.services.EuroService;
+import be.vdab.services.ServicesConfig;
 
 class Main {
 
     public static void main(String[] args) {
-	try (ClassPathXmlApplicationContext context = 
-		 new ClassPathXmlApplicationContext("restclients.xml", "services.xml")) {
-	    System.out.println(
-		    context.getBean(EuroService.class).naarDollar(BigDecimal.valueOf(2)));
+	try (AnnotationConfigApplicationContext context = 
+		new AnnotationConfigApplicationContext(
+			new Class[] { RestClientsConfig.class, ServicesConfig.class })) {
+	    System.out.println(context.getBean(EuroService.class).naarDollar(BigDecimal.valueOf(2)));
 	}
     }
-    
 }
--- src/main/java/be/vdab/services/EuroService.java
+++ src/main/java/be/vdab/services/EuroService.java
@@ -3,16 +3,26 @@ package be.vdab.services;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
 import be.vdab.restclients.KoersenClient;
 
+@Service
 public class EuroService {
 
     private final KoersenClient koersenClient;
 
-    public EuroService(KoersenClient koersenClient) {
+    public EuroService(@Qualifier("ECB") KoersenClient koersenClient) {
 	this.koersenClient = koersenClient;
     }
 
+//    @Autowired
+//    public void setKoersenClient(@Qualifier("ECB") KoersenClient koersenClient) {
+//	this.koersenClient = koersenClient;
+//    }
+    
     public BigDecimal naarDollar(BigDecimal euro) {
 	return euro.multiply(koersenClient.getDollarKoers())
 		.setScale(2, RoundingMode.HALF_UP);
