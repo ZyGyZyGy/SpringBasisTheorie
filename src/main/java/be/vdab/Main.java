21d412f1afa5549b5dacde644a6091ea198270cf
Aleks <Aleks@LOK111-05>
2017-04-25 12:56:09 +0200

    6.2 Meerdere implementaties van een dependency

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -1,17 +1,19 @@
 package be.vdab;
 
+import java.math.BigDecimal;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import be.vdab.services.EuroService;
 
 class Main {
 
     public static void main(String[] args) {
 	try (ClassPathXmlApplicationContext context = 
 		new ClassPathXmlApplicationContext("container.xml")) {
-	    context.getBean("teller1", Teller.class).verhoog();
-	    context.getBean("teller1", Teller.class).verhoog();
-	    Teller teller2 = context.getBean("teller2", Teller.class);
-	    teller2.verhoog();
-	    context.getBean("teller2", Teller.class).verhoog();
+	    System.out.println(
+		    context.getBean(EuroService.class).naarDollar(BigDecimal.valueOf(2)));
 	}
     }
+    
 }
