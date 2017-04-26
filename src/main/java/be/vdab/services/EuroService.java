0b24f7fc44b0a3750f190f3b4d25b88e1d86c369
Alex <jegorov.aleksandr@yahoo.com>
2017-04-26 13:03:07 +0200

    test

--- src/main/java/be/vdab/services/EuroService.java
+++ src/main/java/be/vdab/services/EuroService.java
@@ -3,7 +3,6 @@ package be.vdab.services;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
