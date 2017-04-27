59ffc53246df0c72b4c52fc318f03ce61f10c263
Alex <jegorov.aleksandr@yahoo.com>
2017-04-27 22:40:04 +0200

    11 SPRING EXPRESSION LANGUAGE -  SpEL in XML

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -1,20 +1,15 @@
 package be.vdab;
 
-import java.math.BigDecimal;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import be.vdab.restclients.RestClientsConfig;
-import be.vdab.services.EuroService;
-import be.vdab.services.ServicesConfig;
+import be.vdab.gaming.Spelletje;
 
 class Main {
 
     public static void main(String[] args) {
-	try (AnnotationConfigApplicationContext context = 
-		new AnnotationConfigApplicationContext(
-			new Class[] { RestClientsConfig.class, ServicesConfig.class })) {
-	    System.out.println(context.getBean(EuroService.class).naarDollar(BigDecimal.valueOf(2)));
+	try (ClassPathXmlApplicationContext context =
+		new ClassPathXmlApplicationContext("container.xml")) {
+	    System.out.println(context.getBean(Spelletje.class).getAantalVijanden());
 	}
     }
 }
---
+++ src/main/java/be/vdab/gaming/Spelletje.java
@@ -0,0 +1,15 @@
+package be.vdab.gaming;
+
+public class Spelletje {
+
+    private int aantalVijanden;
+
+    public int getAantalVijanden() {
+	return aantalVijanden;
+    }
+
+    public void setAantalVijanden(int aantalVijanden) {
+	this.aantalVijanden = aantalVijanden;
+    }
+
+}
