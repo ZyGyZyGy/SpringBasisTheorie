92b45bdfb1ca90589a7dcfc13ae5c36850061f83
Alex <jegorov.aleksandr@yahoo.com>
2017-04-27 16:32:46 +0200

    9.4 Java Config en annotations combineren

--- src/main/java/be/vdab/services/ServicesConfig.java
+++ src/main/java/be/vdab/services/ServicesConfig.java
@@ -2,11 +2,13 @@ package be.vdab.services;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import be.vdab.restclients.KoersenClient;
 
 @Configuration
+@ComponentScan
 public class ServicesConfig {
 
     @Bean
