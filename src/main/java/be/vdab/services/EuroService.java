a42758a08c2a0da3ca172eddf28625f8f6dbda35
Alex <jegorov.aleksandr@yahoo.com>
2017-04-27 16:34:42 +0200

    Gewoon Java config - geen overbodige bs

--- src/main/java/be/vdab/services/EuroService.java
+++ src/main/java/be/vdab/services/EuroService.java
@@ -3,18 +3,13 @@ package be.vdab.services;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
-
 import be.vdab.restclients.KoersenClient;
-
-@Service
 
 public class EuroService {
 
     private final KoersenClient koersenClient;
 
-    public EuroService(@Qualifier("ECB") KoersenClient koersenClient) {
+    public EuroService(KoersenClient koersenClient) {
 	this.koersenClient = koersenClient;
     }
     
